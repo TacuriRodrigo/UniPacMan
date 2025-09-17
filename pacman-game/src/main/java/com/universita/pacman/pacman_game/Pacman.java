47051d1dd31e7ad12ecfc81ee74ceba6a98ecb72
TacuriRodrigo <145766154+TacuriRodrigo@users.noreply.github.com>
2025-09-17 18:45:23 +0200

    Create Pacman.java

---
+++ pacman-game/src/main/java/com/universita/pacman/pacman_game/Pacman.java
@@ -0,0 +1,12 @@
+package com.universita.pacman.pacman_game;
+
+public class Pacman extends Entità{
+	
+	public Pacman(double Xini, double Yini){
+		super (Xini,Yini);
+		setPman(Xini, Yini);
+		
+		
+	}
+
+}
