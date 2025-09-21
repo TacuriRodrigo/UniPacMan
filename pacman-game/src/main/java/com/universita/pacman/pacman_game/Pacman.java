f2693c194ccb94455e4f59bcbc555b56965a894e
TacuriRodrigo <145766154+TacuriRodrigo@users.noreply.github.com>
2025-09-21 17:37:34 +0200

    Classe pacman

--- pacman-game/src/main/java/com/universita/pacman/pacman_game/Pacman.java
+++ pacman-game/src/main/java/com/universita/pacman/pacman_game/Pacman.java
@@ -5,8 +5,56 @@ public class Pacman extends Entità{
 	public Pacman(double Xini, double Yini){
 		super (Xini,Yini);
 		setPman(Xini, Yini);
-		
-		
 	}
+	
+	public void setSu() {
+		deltaX=0; 
+		deltaY=-1;
+	}
+	
+	public void setGiù() {
+		deltaX=0;
+		deltaY=1;
+	}
+	
+	public void setDx() {
+		deltaX=1;
+		deltaY=0;
+	}
+	
+	public void setSx() {
+		deltaX=-1;
+		deltaY=0;
+	}
+	
+	public double getXInd() {
+		return this.x;
+	}
+	
+	public double getYInd() {
+		return this.y;
+	} 
+	
+	public void setXind(double nx) {
+		this.x=nx;
+	}
+	
+	public void setYind(double ny) {
+		this.y=ny;
+	}
+	
+	public void OdeltaXY() {
+		deltaX=0;
+		deltaY=0;
+	}
+	
+	/* metodo per il test */
+	
+	public void updatePos() {
+		this.x+=deltaX;
+		this.y+=deltaY;
+	}
+	
+	/************************/
 
 }
