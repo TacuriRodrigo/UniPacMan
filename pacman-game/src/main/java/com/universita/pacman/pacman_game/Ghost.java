9cee7f2adf7c2fc18f58ad9c7d260e54ce6b490d
TacuriRodrigo <145766154+TacuriRodrigo@users.noreply.github.com>
2025-09-25 23:47:57 +0200

    Update Ghost.java

--- pacman-game/src/main/java/com/universita/pacman/pacman_game/Ghost.java
+++ pacman-game/src/main/java/com/universita/pacman/pacman_game/Ghost.java
@@ -1,5 +1,19 @@
 package com.universita.pacman.pacman_game;
 
-public class Ghost {
+public class Ghost extends Entità{
+	
+	public boolean startProcess;
+	
+	public Ghost(double xi,double yi){
+		super(xi,yi);
+		
+		startProcess=false;
+	}
+	/*
+	private boolean eatPacman() {
+		return Math.sqrt(Math.pow(this.x-pacmanX, 2 )+Math.pow(this.y-pacmanY, 2))<15;
+	}
+	
+	*/
 
 }
