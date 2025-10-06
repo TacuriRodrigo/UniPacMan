0db74346a8d7a91279cf22a41abe2943504a3d91
TacuriRodrigo <145766154+TacuriRodrigo@users.noreply.github.com>
2025-10-06 17:37:05 +0200

    Update Mappa.java

--- pacman-game/src/main/java/com/universita/pacman/pacman_game/Mappa.java
+++ pacman-game/src/main/java/com/universita/pacman/pacman_game/Mappa.java
@@ -1,9 +1,18 @@
 package com.universita.pacman.pacman_game;
 
+import java.io.IOException;
+
 public class Mappa {
-	final int W=1;//muro
-	final int F=2;//cibo 
-	final int E=3;//vuoto
+	private Pacman pacman;
+	final int W=1;	//Muro
+	final int F=2;	//Cibo 
+	final int E=3;	//Vuoto
+	int score;
+	int lives;
+	private double squareHeight;
+	private double squareWidth;
+	private int mapHeight;
+	private int mapWidth;
 	private int board[][] = {
 			//-----------------------X-----------------------------//
 			{W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W},
@@ -39,5 +48,41 @@ public class Mappa {
 			{W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W}
 	};
 	
+	public Mappa() throws IOException {
+		score=0;
+		lives = 3;
+		mapHeight=416;
+		mapWidth=429;
+		squareHeight=mapHeight/31;
+		squareWidth=mapWidth/429;
+		
+	}
+	 public	Pacman placePacman() {
+		 return new Pacman(13*squareWidth+squareWidth/2,22.5*(squareHeight)+squareHeight/2);
+	}
+	 
+	public int getMapWidth() {
+		return mapWidth;
+	}
 	
+	public int getMapHeight() {
+		return mapHeight;
+	}
+	
+	public String toString(){
+		String s="";
+		for(int i=0;i<board.length;i++) {
+			for(int j=0;j<board[i].length;j++) {
+				s = s+","+board[i][j];
+			}
+			s=s+"\n";
+		}
+		return s;
+	}
+	
+	public boolean isClear(int i,int j ) {
+		return !(board[i][j]==1);
+	}
+	
+	 
 }
